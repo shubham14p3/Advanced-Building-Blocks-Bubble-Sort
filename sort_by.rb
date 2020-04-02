3c95bdf3510ae65c579f8ce9db8b9d53cb9b1dbf
shubham14p3 <shubham14p3@gmail.com>
2020-04-03 03:03:53 +0530

    Added Bubble Sort by defination

---
+++ sort_by.rb
@@ -0,0 +1,38 @@
+def bubble_sort(array)
+	n = array.length
+	swapped = true	
+		while swapped do
+		swapped = false
+		(n - 1).times do |i|
+			if array[i] > array[i + 1]
+			array[i], array[i + 1] = array[i + 1], array[i]
+			swapped = true
+			end
+		end
+	end
+	puts "This is the soreted list #{array}"
+end
+	array = [5,9,7,3,1]
+	# bubble_sort(array)
+
+def bubble_sort_by(array)
+	passes = array.length
+	while passes > 0
+		swapped = 1
+		while swapped < passes do
+			if yield(array[swapped-1],array[swapped]) < 0
+				array[swapped - 1], array[swapped] = array[swapped], array[swapped-1]
+			end
+			swapped += 1
+		end
+		passes -= 1
+	end
+	puts "This is the soreted list after sorting : #{array}"
+end
+
+array = ["hi","hello","hey"]
+puts "This is the list that needs to be sorted : #{array}"
+
+bubble_sort_by(array) do |left,right|
+	right.length - left.length
+end
